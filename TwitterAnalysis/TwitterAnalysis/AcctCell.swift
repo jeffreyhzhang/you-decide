9fdcef3785e20b93cc3fa6e02d656d9d37d257c9
jeffreyhzhang <jeffreyhzhang@users.noreply.github.com>
2015-05-03 12:47:51 -0400

    Committed by Udacity

---
+++ TwitterAnalysis/TwitterAnalysis/AcctCell.swift
@@ -0,0 +1,17 @@
+//
+//  AcctCell.swift
+//  TwitterAnalysis
+//
+//  Created by JeffreyLee on 4/27/15.
+//  Copyright (c) 2015 Jeffrey. All rights reserved.
+//
+
+import UIKit
+
+class AcctCell: UITableViewCell{
+    
+    @IBOutlet weak var imgvw: UIImageView!
+    @IBOutlet weak var lblAccount: UILabel!
+    @IBOutlet weak var lblFollowers: UILabel!
+
+}
