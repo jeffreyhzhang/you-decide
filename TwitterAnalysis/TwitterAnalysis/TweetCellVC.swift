5d270ea892e041bc7a46aedc8d400843d3c13c21
jeffreyhzhang <jeffreyhzhang@users.noreply.github.com>
2015-05-03 12:47:58 -0400

    Committed by Udacity

---
+++ TwitterAnalysis/TwitterAnalysis/TweetCellVC.swift
@@ -0,0 +1,18 @@
+//
+//  TweetCellVC.swift
+//
+//  TwitterAnalysis
+//
+//  Created by Jeffrey Zhang on 4/24/15.
+//  Copyright (c) 2015   All rights reserved.
+//
+
+
+import UIKit
+
+class TweetCellVC: UITableViewCell {
+
+    @IBOutlet weak var imghdlt:UIImageView!
+    @IBOutlet weak var lblheader: UILabel!
+    @IBOutlet weak var lblTweet: UILabel!
+}
